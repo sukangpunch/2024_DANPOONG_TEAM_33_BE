31111e0421c1ff5e09de527fc20b65d95e3f3e2a
Hwangbo Jaeyun <hwangbo0319@gmail.com>
2024-11-18 04:07:46 +0900

    [🛠️]  app.py 파일 수정

--- Flask/app.py
+++ Flask/app.py
@@ -9,15 +9,21 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 from dotenv import load_dotenv
-
 load_dotenv()
 
-FLASK_PORT = os.environ.get('FLASK_PORT')
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 ########################################################################################################################################################################################################################    
-# MONGODB 연결
+# Utis 함수
+
+from utils.jwt import decode_token
+########################################################################################################################################################################################################################    
+# MongoDB 연결
 
 MONGODB_URI = os.environ.get('MONGODB_URI')
 MONGODB_DB = os.environ.get('MONGODB_DB')
+
+# MongoDB Collections
+COLLECTION_CMI = os.environ.get('COLLECTION_CMI')
 
 client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
 db = client[MONGODB_DB]
@@ -43,6 +49,8 @@ def get_cursor():
     return g.cursor
 ########################################################################################################################################################################################################################    
 # FLASK 설정
+
+FLASK_PORT = os.environ.get('FLASK_PORT')
 
 app = Flask(__name__)
 
@@ -77,8 +85,42 @@ class PingMongo(Resource):
             "message": db.command("ping")
         }
 ########################################################################################################################################################################################################################    
-@company_list.route('/recent')
+@company_list.route('/recent/page/main')
 class RecentCompanyList(Resource):
+    @company_list.header('Authorization', 'JWT 토큰', required = False)
+    
+    def get(self):
+        collection = db[COLLECTION_CMI]
+        auth_header = request.headers.get('Authorization')
+
+        feild = {
+            '_id': 0,
+            'info_no': 1,
+            'company.name': 1, 
+            'certifications.essential': 1,
+        }
+
+        # 로그인 상태가 아니거나 토큰이 없는 경우
+        if not auth_header:
+            company_list = collection.find({}, feild).sort("hiring_period.start_date.$date", -1).limit(6)
+            company_list = list(company_list)
+
+            # 'essential' 필드 리스트화
+            for company in company_list:
+                certifications = company.get("certifications", {})
+                
+                if "essential" in certifications and isinstance(certifications["essential"], list):
+                    certifications["essential"] = [cert.get("name", "") for cert in certifications["essential"]]
+                
+                # 업데이트된 certifications 저장
+                company["certifications"] = certifications
+        
+        user_id = decode_token(JWT_SECRET_KEY, auth_header)
+
+        return {
+            "status": "success",
+            "data": result
+        }
 
 
 ########################################################################################################################################################################################################################    
