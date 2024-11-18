c5efd45b851fff6b883e15c8efea53a091141bc6
Hwangbo Jaeyun <hwangbo0319@gmail.com>
2024-11-18 19:58:36 +0900

    [🛠️] jwt.py 파일 수정
    + Debug 용 print 문 추가

--- Flask/utils/jwt.py
+++ Flask/utils/jwt.py
@@ -3,16 +3,14 @@ from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
 
 def decode_token(JWT_SECRET_KEY, auth_header):
 
-    # 헤더에 Authorization 토큰이 없거나 잘못된 경우 처리
-    if not auth_header or not auth_header.startswith("Bearer "):
-        return 400
-
     # "Bearer " 이후의 실제 토큰 값 추출
     token = auth_header[len("Bearer "):].strip()
+    print(f"Decoded token: {token}")  # 디버깅용 로그
 
     try:
         # JWT 토큰 디코딩
         decoded_payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=["HS256"])
+        print(f"Decoded payload: {decoded_payload}")  # 디버깅용 로그
 
         # 디코딩된 페이로드에서 user_id(PK) 추출
         user_id = decoded_payload.get("id")
@@ -22,6 +20,8 @@ def decode_token(JWT_SECRET_KEY, auth_header):
         # 성공적으로 user_id 반환
         return user_id
     except ExpiredSignatureError:
+        print("Expired token")  # 디버깅용 로그
         return 401
     except InvalidTokenError:
+        print("Invalid token")  # 디버깅용 로그
         return 401
