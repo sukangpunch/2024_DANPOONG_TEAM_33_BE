a4c2ac84c2858d4a099def53b6cbb13f45475fba
Hwangbo Jaeyun <hwangbo0319@gmail.com>
2024-11-18 19:57:42 +0900

    [🛠️] date.py 파일 수정
    + alculate_d_day 함수 추가

--- Flask/utils/date.py
+++ Flask/utils/date.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def add_d_day_to_companies(company_list):
     """
     회사 리스트에 D-day 값을 추가.
@@ -13,3 +15,30 @@ def add_d_day_to_companies(company_list):
         if end_date:
             company["hiring_period"]["D-day"] = calculate_d_day(end_date)
     return company_list
+
+def calculate_d_day(date_str):
+    """
+    주어진 날짜 문자열을 날짜 객체로 변환하고 오늘을 기준으로 D-day 계산.
+    
+    Args:
+        date_str (str): "YYYY-MM-DD" 형식의 날짜 문자열.
+
+    Returns:
+        int: 오늘 기준으로 몇 일 뒤인지 (양수: 미래, 음수: 과거, 0: 오늘).
+    """
+    target_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+    today = datetime.now().date()
+    d_day = (target_date - today).days
+    return d_day
+
+def serialize_company_list(company_list):
+
+    for company in company_list:
+        # 날짜를 문자열로 변환
+        if 'hiring_period' in company and 'end_date' in company['hiring_period']:
+            company['hiring_period']['end_date'] = company['hiring_period']['end_date'].isoformat()
+        elif 'hiring_period' in company and 'start_date' in company['hiring_period']:
+            company['hiring_period']['start_date'] = company['hiring_period']['start_date'].isoformat()
+
+    return company_list
+
