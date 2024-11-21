714bffe88bab8b695ca97dba47a211da695e6167
khjdeveloper <makernari.popora99@gmail.com>
2024-11-21 21:04:11 +0900

    init 애플리케이션 이름 수정

---
+++ onetry/src/main/java/com/example/onetry/OnetryApplication.java
@@ -0,0 +1,13 @@
+package com.example.onetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnetryApplication.class, args);
+    }
+
+}
